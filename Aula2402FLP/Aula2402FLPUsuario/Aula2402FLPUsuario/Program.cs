66d2ca796aa160c158eccf22c9c8d86aba0441bf
Grasy R <159390727+grasyzip@users.noreply.github.com>
2025-02-28 21:37:38 -0300

    Add files via upload

---
+++ Aula2402FLP/Aula2402FLPUsuario/Aula2402FLPUsuario/Program.cs
@@ -0,0 +1,38 @@
+﻿
+double a, b, c, delta, raiz1, raiz2;
+
+Console.WriteLine("Digite o valor de a:");
+    while (!double.TryParse(Console.ReadLine(), out a))
+    {
+        Console.WriteLine("Valor inválido. Digite um número:");
+    }
+
+Console.WriteLine("Digite o valor de b:");
+    while (!double.TryParse(Console.ReadLine(), out b))
+    {
+        Console.WriteLine("Valor inválido. Digite um número:");
+    }
+
+Console.WriteLine("Digite o valor de c:");
+    while (!double.TryParse(Console.ReadLine(), out c))
+    {
+        Console.WriteLine("Valor inválido. Digite um número:");
+    }
+
+    delta = b * b - 4 * a * c;
+
+    if (delta < 0)
+    {
+        Console.WriteLine("A equação não possui raízes reais.");
+    } else if (delta == 0)
+    {
+        double raiz = -b / (2 * a);
+        Console.WriteLine($"A equação possui uma raiz real dupla: {raiz:F2}");
+    } else
+    {
+        raiz1 = (-b + Math.Sqrt(delta)) / (2 * a);
+        raiz2 = (-b - Math.Sqrt(delta)) / (2 * a);
+        Console.WriteLine($"As raízes da equação são: {raiz1:F2} e {raiz2:F2}");
+    }
+
+   Console.ReadKey();
